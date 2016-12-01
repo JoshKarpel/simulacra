49655888c9069c4aac67708a93644221c91f3c05
JoshKarpel <josh.karpel@gmail.com>
2016-12-01 00:16:01 -0600

    Wrote deploy.py to help when deploying code on the cluster and elsewhere.

---
+++ deploy.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+
+
+def blacklist(directory, contents):
+    return tuple(p for p in contents if not os.path.isfile(os.path.join(directory, p)))
+
+
+if __name__ == '__main__':
+    shutil.rmtree('deploy')
+
+    for d in (d for d in os.listdir(os.getcwd()) if not d.startswith('.') and not d == 'deploy' and os.path.isdir(d)):
+        t = os.path.join('deploy', d)
+        shutil.copytree(d, t, ignore = blacklist)
+
+    os.chdir('deploy')
+    for d in os.listdir(os.getcwd()):
+        shutil.make_archive(d, format = 'gztar', base_dir = d)
+
+    os.chdir('..')
+    shutil.copy('ionization/scripts/run_sim.py', 'deploy')
+    shutil.copy('ionization/scripts/run_sim.sh', 'deploy')
+    shutil.copy('ionization/scripts/middleman.py', 'deploy')
+    shutil.copy('compy/scripts/tar.py', 'deploy')
