e8245824046e41e885f93e6a10938ec2216772f6
Josh Karpel <josh.karpel@gmail.com>
2017-05-12 19:54:06 -0500

    Messy, messy...

--- src/simulacra/__init__.py
+++ src/simulacra/__init__.py
@@ -25,16 +25,7 @@ mpl_rcParams_update = {
 
 matplotlib.rcParams.update(mpl_rcParams_update)
 
-# set up platform-independent runtime cython compilation and imports
 import numpy as _np
-import pyximport
-
-
-pyx_dir = os.path.join(os.path.dirname(__file__), '.pyxbld')
-pyximport.install(setup_args = {"include_dirs": _np.get_include()},
-                  build_dir = pyx_dir,
-                  language_level = 3)
-
 _np.set_printoptions(linewidth = 200)  # screw character limits
 
 from simulacra.core import *
