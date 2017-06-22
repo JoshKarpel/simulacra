b4a6b033fc98b69405c31eb481357a4f8a99a87d
Josh Karpel <josh.karpel@gmail.com>
2017-06-22 08:40:04 -0500

    Added copyright notice to all source files.

--- src/simulacra/vis.py
+++ src/simulacra/vis.py
@@ -1,3 +1,22 @@
+"""
+Simulacra visualization sub-package.
+
+
+Copyright 2017 Josh Karpel
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import itertools
 import os
 import logging
@@ -152,7 +171,7 @@ def get_figure(fig_scale = 0.95, fig_dpi_scale = 1, aspect_ratio = GOLDEN_RATIO,
     elif fig_scale == 'half':
         fig_scale = .475
 
-    fig = plt.figure(figsize = _get_fig_dims(fig_scale, fig_width_pts = fig_width_pts, aspect_ratio = aspect_ratio), dpi = fig_dpi_scale * 100)
+    fig = plt.figure(figsize = _get_fig_dims(fig_scale, fig_width_pts = fig_width_pts, aspect_ratio = aspect_ratio), dpi = fig_dpi_scale * 600)
 
     return fig
 
@@ -194,7 +213,7 @@ def save_current_figure(name,
 
     if target_dir is None:
         target_dir = os.getcwd()
-    path = os.path.join(target_dir, '{}{}.{}'.format(name, name_postfix, img_format))
+    path = os.path.join(target_dir, utils.strip_illegal_characters('{}{}.{}'.format(name, name_postfix, img_format)))
 
     utils.ensure_dir_exists(path)
 
@@ -224,19 +243,35 @@ class FigureManager:
         Parameters
         ----------
         name
+            The desired file name for the plot.
         name_postfix
+            An additional postfix for the file name.
         fig_scale
+            The relative width of the figure.
         fig_dpi_scale
+            The relative point density of the figure (i.e., 2 is twice as much dpi as normal).
         fig_width_pts
+            The width of the figure, in points (72 points per inch).
         aspect_ratio
+            The aspect ratio of the figure (height / width).
         target_dir
+            The directory to save the plot to.
         img_format
+            The format for the plot. Accepts any matplotlib file format.
         img_scale
+            The scale for the image.
+        tight_layout
+            If ``True``, uses matplotlib's tight layout option before saving.
         close_before_enter
+            If ``True``, close whatever matplotlib plot is open before trying to create the new figure.
         close_after_exit
+            If ``True``, close the figure after exiting the context manager.
         save_on_exit
+            If ``True``, save the figure after exiting the context manager.
         show
+            If ``True``, show the figure after exiting the context manager.
         kwargs
+            Keyword arguments are silently absorbed.
         """
         self.name = name
         self.name_postfix = name_postfix
@@ -556,9 +591,11 @@ def xy_plot(name,
         If ``True``, the x and y data for the plot will be saved to a CSV file with the same name in the target directory.
     kwargs
         Keyword arguments are passed to :class:`FigureManager`.
+
     Returns
     -------
-
+    :class:`FigureManager`
+        The :class:`FigureManager` that the xy-plot was constructed in.
     """
     # set up figure and axis
     if figure_manager is None:
@@ -683,45 +720,6 @@ def xxyy_plot(name,
               grid_kwargs = None, minor_grid_kwargs = None,
               save_csv = False,
               **kwargs):
-    """
-    Generate and save a generic xx-yy plot.
-
-    :param name: filename for the plot
-    :param x_data: a single array to plot the y data against
-    :param y_data: any number of arrays of the same length as x_data to plot
-    :param line_labels: the labels for the lines
-    :param line_kwargs: other keyword arguments for each line's .plot() call (None for default)
-    :param x_unit: a number to scale the x_data by. Can be a string corresponding to a key in the unit name/value dict.
-    :param y_unit: a number to scale the y_data by. Can be a string corresponding to a key in the unit name/value dict.
-    :param x_log_axis: if True, log the x axis
-    :param y_log_axis: if True, log the y axis
-    :param x_lower_limit: lower limit for the x axis, defaults to np.nanmin(x_data)
-    :param x_upper_limit: upper limit for the x axis, defaults to np.nanmax(x_data)
-    :param y_lower_limit: lower limit for the y axis, defaults to min(np.nanmin(y_data))
-    :param y_upper_limit: upper limit for the y axis, defaults to min(np.nanmin(y_data))
-    :param vlines: a list of x positions to place vertical lines
-    :param vline_kwargs: a list of kwargs for each vline (None for default)
-    :param hlines: a list of y positions to place horizontal lines
-    :param hline_kwargs: a list of kwargs for each hline (None for default)
-    :param x_extra_ticks:
-    :param x_extra_tick_labels:
-    :param y_extra_ticks:
-    :param y_extra_tick_labels:
-    :param title: a title for the plot
-    :param x_label: a label for the x axis
-    :param y_label: a label for the y axis
-    :param font_size_title: font size for the title
-    :param font_size_axis_labels: font size for the axis labels
-    :param font_size_tick_labels: font size for the tick labels
-    :param font_size_legend: font size for the legend
-    :param ticks_on_top:
-    :param ticks_on_right:
-    :param legend_on_right:
-    :param grid_kwargs:
-    :param save_csv: if True, save x_data and y_data to a CSV file
-    :param kwargs: kwargs are passed to a FigureManager context manager
-    :return: the path the plot was saved to
-    """
     # set up figure and axis
     if figure_manager is None:
         figure_manager = FigureManager(name, **kwargs)
@@ -1007,51 +1005,6 @@ def xyt_plot(name,
              save_csv = False,
              progress_bar = True,
              **kwargs):
-    """
-
-    :param name: filename for the plot
-    :param x_data: a single array to ploy the y values against
-    :param t_data: a single array whose values will be animated over
-    :param y_funcs: functions of the form ``f(x, t, **kwargs)``, where kwargs are from `y_func_kwargs`
-    :param y_func_kwargs: keyword arguments for the y_funcs
-    :param line_labels: the labels for the lines
-    :param line_kwargs: other keyword arguments for each line's .plot() call (None for default)
-    :param figure_manager:
-    :param x_unit:
-    :param y_unit:
-    :param t_unit:
-    :param t_fmt_string:
-    :param t_text_kwargs:
-    :param x_log_axis:
-    :param y_log_axis:
-    :param x_lower_limit:
-    :param x_upper_limit:
-    :param y_lower_limit:
-    :param y_upper_limit:
-    :param vlines:
-    :param vline_kwargs:
-    :param hlines:
-    :param hline_kwargs:
-    :param x_extra_ticks:
-    :param y_extra_ticks:
-    :param x_extra_tick_labels:
-    :param y_extra_tick_labels:
-    :param title:
-    :param x_label:
-    :param y_label:
-    :param font_size_title:
-    :param font_size_axis_labels:
-    :param font_size_tick_labels:
-    :param font_size_legend:
-    :param ticks_on_top:
-    :param ticks_on_right:
-    :param legend_on_right:
-    :param grid_kwargs:
-    :param length:
-    :param save_csv:
-    :param kwargs:
-    :return:
-    """
     # set up figure and axis
     if figure_manager is None:
         figure_manager = FigureManager(name, save_on_exit = False, fig_dpi_scale = fig_dpi_scale, **kwargs)
@@ -1501,7 +1454,6 @@ class Animator:
                  length = 60, fps = 30,
                  colormap = plt.cm.get_cmap('inferno')):
         """
-
         Parameters
         ----------
         postfix : :class:`str`
@@ -1653,6 +1605,12 @@ class Animator:
 
 
 class RichardsonColormap(matplotlib.colors.Colormap):
+    """
+    A matplotlib Colormap subclass which implements the colormap described in J. L. Richardson, Comput. Phys. Commun. 63, 84 (1991).
+
+    This colormap is appropriate for visualizing complex-valued functions in two dimensions.
+    """
+
     def __init__(self):
         self.name = 'richardson'
         self.N = 256
@@ -1665,14 +1623,14 @@ class RichardsonColormap(matplotlib.colors.Colormap):
         zplus = z + 2
         eta = np.where(np.greater_equal(z, 0), 1, -1)
 
-        common = .5 + (eta * (.5 - (mag / zplus)))  # common term to rgb
+        common = .5 + (eta * (.5 - (mag / zplus)))
         real_term = real / (np.sqrt(6) * zplus)
         imag_term = imag / (np.sqrt(2) * zplus)
 
-        rgba = np.ones(np.shape(x) + (4,))  # create rgba array of shape shape as x, except in last dimension, where rgba values will be stored
-        rgba[:, 0] = common + (2 * real_term)  # red
-        rgba[:, 1] = common - real_term + imag_term  # green
-        rgba[:, 2] = common - real_term - imag_term  # blue
+        rgba = np.ones(np.shape(x) + (4,))              # create rgba array of shape shape as x, except in last dimension, where rgba values will be stored
+        rgba[:, 0] = common + (2 * real_term)           # red
+        rgba[:, 1] = common - real_term + imag_term     # green
+        rgba[:, 2] = common - real_term - imag_term     # blue
 
         return rgba
 
@@ -1683,6 +1641,7 @@ CMAP_TO_OPPOSITE['richardson'] = WHITE
 
 
 class RichardsonNormalization(matplotlib.colors.Normalize):
+    """A matplotlib Normalize subclass which implements an appropriate normalization for :class:`RichardsonColormap`."""
     def __init__(self, equator_magnitude = 1):
         self.equator_magnitude = np.abs(equator_magnitude)
 
